5cf65d8d9443c9e54c83f6048e1923b98fef6d63
4733 <939502270@qq.com>
2024-09-13 22:40:54 +0800

    辅助判空工具

--- nulltest/src/main/java/com/example/nulltest/NullTest.kt
+++ nulltest/src/main/java/com/example/nulltest/NullTest.kt
@@ -1,12 +1,31 @@
 package com.example.nulltest
 
 import com.example.demo1.Study
+var content:String?="hello"
 
 fun main(){
     doStudy(null)
+
+    if (content!=null){
+        printUpperCase()
+    }
 }
 
-fun doStudy(study: Study?) {
-    study?.readBooks()
-    study?.doHomework()
+//fun doStudy(study: Study?) {
+//    study?.readBooks()
+//    study?.doHomework()
+//}
+
+fun doStudy(study: Study?){
+    study?.let{
+        it.readBooks()
+        it.doHomework()
+    }
+}
+
+fun getTextLength(text:String?)= text?.length?:0
+
+fun printUpperCase(){
+    val upperCase = content!!.toUpperCase()
+    println(upperCase)
 }
