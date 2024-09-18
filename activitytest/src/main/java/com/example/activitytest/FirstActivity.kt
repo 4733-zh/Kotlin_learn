5acfbed57ae6f6c48c0bd80a706f99ac648ba85b
4733 <939502270@qq.com>
2024-09-18 22:25:28 +0800

    activitytest在Activity中使用Menu

--- activitytest/src/main/java/com/example/activitytest/FirstActivity.kt
+++ activitytest/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -1,6 +1,8 @@
 package com.example.activitytest
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +13,20 @@ import com.example.activitytest.databinding.FirstLayoutBinding
 
 class FirstActivity : AppCompatActivity() {
     private lateinit var binding: FirstLayoutBinding
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.add_item->Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show()
+            R.id.remove_item->Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FirstLayoutBinding.inflate(layoutInflater)
