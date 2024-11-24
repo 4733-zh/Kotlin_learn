7c2a36c9c3e71c333be9b9587ae71cac778e914c
4733 <939502270@qq.com>
2024-11-24 15:01:42 +0800

    listviewtest提升listView的效率，自定义item，添加点击事件

--- listviewtest/src/main/java/com/example/listviewtest/MainActivity.kt
+++ listviewtest/src/main/java/com/example/listviewtest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.listviewtest
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.listviewtest.databinding.ActivityMainBinding
 
@@ -10,13 +11,32 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val data= listOf( "Apple", "Banana", "Orange","Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango","Apple", "Banana", "Orange","Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango")
+    private val fruitList = ArrayList<Fruit>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initFruits()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data)
+        val adapter = FruitAdapter(this,R.layout.fruit_item,fruitList)
         binding.listView.adapter = adapter
+        binding.listView.setOnItemClickListener { _, _, position, _ ->
+            val fruit = fruitList[position]
+            Toast.makeText(this,fruit.name,Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun initFruits(){
+        repeat(2) {
+            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
+            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
+            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
+            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
+            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
+            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
+            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
+            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
+            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
+            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+        }
     }
 }
