9d02ebc9603b7316e9f5a463ae894bdc93c68e84
zhouheng <939502270@qq.com>
2025-04-01 21:56:27 +0800

    新建RecyclerViewTest项目,学习基本用法

---
+++ recyclerviewtest/src/main/java/com/example/recyclerviewtest/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.recyclerviewtest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.relativelayouttest.R
+import com.example.relativelayouttest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val tag = "MainActivity"
+    private val fruitList = ArrayList<Fruit>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initFruits()// 初始化数据
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        val adapter = FruitAdapter(fruitList)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun initFruits() {
+        repeat(2){
+            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
+            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
+            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
+            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
+            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
+            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
+            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
+            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
+            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
+            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+        }
+    }
+}
--- relativelayouttest/src/main/java/com/example/relativelayouttest/MainActivity.kt
+++ relativelayouttest/src/main/java/com/example/relativelayouttest/MainActivity.kt
@@ -5,10 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.relativelayouttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
